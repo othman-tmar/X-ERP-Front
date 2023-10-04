ae7e23db7fd1582609bc75aa81c6ed81e7ff3b91
othman-tmar <120841106+othman-tmar@users.noreply.github.com>
2023-10-04 13:48:39 +0100

    Update Api.jsx

--- src/axios/Api.jsx
+++ src/axios/Api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'http://localhost:3002/api';
+axios.defaults.baseURL = 'https://erp-back.onrender.com';
 
 
 export function getAxiosInstance() {
